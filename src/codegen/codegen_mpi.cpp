a22b849c08294e262b9f155ce5f483df5a85027a
NikosPappas <nikolaosgpappast@gmail.com>
2026-02-17 21:49:16 +0200

    Update codegen_mpi.cpp

--- src/codegen/codegen_mpi.cpp
+++ src/codegen/codegen_mpi.cpp
@@ -1,6 +1,6 @@
 /*
  * PhysCC - Domain-Specific Compiler for Physics Simulations
- * Copyright (C) 2026  [Το Όνομά Σας]
+ * Copyright (C) 2026 Nikos Pappas
  * * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
